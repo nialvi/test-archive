697f42312e9f448b3e48cdedf5fb9799087e4f36
Nikolaev Viktor <maclauddd@gmail.com>
2020-09-18 08:42:49 +0300

    added simple test framework

---
+++ src/testing/3-framework.js
@@ -0,0 +1,36 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+
+test("sum add numbers", () => {
+  const result = sum(1, 2);
+  const expected = 3;
+
+  expect(result).toBe(expected);
+});
+
+test("subtract subtracts numbers", () => {
+  const result = subtract(2, 1);
+  const expected = 1;
+
+  expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✓ ${title}`);
+  } catch (error) {
+    console.error(`✕ ${title}`);
+    console.error(error);
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+  };
+}
